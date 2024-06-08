11733d059b5c6321b5fb8f62dba9b187ec1589c5
VictorReami <victor.mreami@gmail.com>
2024-06-08 11:42:15 -0300

    5.9 - Iniciando com offset

--- validador-boleto/src/main/java/br/com/reami/validador_boleto/Service/Kafka/BoletoConsumer.java
+++ validador-boleto/src/main/java/br/com/reami/validador_boleto/Service/Kafka/BoletoConsumer.java
@@ -1,13 +1,17 @@
 package br.com.reami.validador_boleto.Service.Kafka;
 
 import br.com.reami.avro.Boleto;
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.listener.ConsumerSeekAware;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
-public class BoletoConsumer {
+public class BoletoConsumer implements ConsumerSeekAware {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BoletoConsumer.class);
 
@@ -15,5 +19,10 @@ public class BoletoConsumer {
     public void consomeBoleto(Boleto boleto) throws InterruptedException {
         LOGGER.info(String.format("Consumindo mensagem -> %s", boleto));
         Thread.sleep(3000);
+    }
+
+    @Override
+    public void onPartitionsAssigned(Map<TopicPartition, Long> assignments, ConsumerSeekCallback callback) {
+        assignments.forEach((t, o) -> callback.seekToEnd(t.topic(), t.partition()));
     }
 }
