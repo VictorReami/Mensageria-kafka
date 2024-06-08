ec3d4003abbaf7f9cf2311b816909cc767c35600
VictorReami <victor.mreami@gmail.com>
2024-06-08 11:04:16 -0300

    5.5 - Consumindo mensagem do kafka

--- validador-boleto/src/main/java/br/com/reami/validador_boleto/Service/Kafka/BoletoConsumer.java
+++ validador-boleto/src/main/java/br/com/reami/validador_boleto/Service/Kafka/BoletoConsumer.java
@@ -1,4 +1,18 @@
 package br.com.reami.validador_boleto.Service.Kafka;
 
+import br.com.reami.avro.Boleto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BoletoConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BoletoConsumer.class);
+
+    @KafkaListener(topics = "${spring.kafka.topico-boleto}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consomeBoleto(Boleto boleto){
+        LOGGER.info(String.format("Consumindo mensagem -> %s", boleto));
+    }
 }
