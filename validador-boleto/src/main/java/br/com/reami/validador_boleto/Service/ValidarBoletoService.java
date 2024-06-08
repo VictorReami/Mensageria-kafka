99465bf70a75730c8ab58178878073e9eb50afac
VictorReami <victor.mreami@gmail.com>
2024-06-08 12:10:41 -0300

    5.12 - Continuando com a validação do boleto

--- validador-boleto/src/main/java/br/com/reami/validador_boleto/Service/ValidarBoletoService.java
+++ validador-boleto/src/main/java/br/com/reami/validador_boleto/Service/ValidarBoletoService.java
@@ -1,13 +1,47 @@
 package br.com.reami.validador_boleto.Service;
 
 import br.com.reami.validador_boleto.Entity.BoletoEntity;
+import br.com.reami.validador_boleto.Enums.SituacaoBoleto;
+import br.com.reami.validador_boleto.Repository.BoletoRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class ValidarBoletoService {
 
-    public void validar(BoletoEntity boleto){
+    private final BoletoRepository boletoRepository;
 
+    public ValidarBoletoService(BoletoRepository boletoRepository) {
+        this.boletoRepository = boletoRepository;
+    }
+
+    public void validar(BoletoEntity boleto){
+        var codigo = Integer.parseInt(boleto.getCodigoBarras().substring(0,1));
+
+        if(codigo % 2 == 0){
+            //Todo boleto invalido
+            complementarBoletoErro(boleto);
+
+            //Salvar na base
+            boletoRepository.save(boleto);
+
+            //Notificar
+
+        }else{
+            //Todo boleto valido
+            //Salvar em base
+            //Notifica que o boleto foi validado com sucesso
+            //Seguir com o pagamento
+        }
 
     }
+
+    private void complementarBoletoErro(BoletoEntity boleto){
+        boleto.setDataCriacao(LocalDateTime.now());
+        boleto.setDataAtualizacao(LocalDateTime.now());
+        boleto.setSituacaoBoleto(SituacaoBoleto.ERRO_VALIDACAO);
+    }
+
+
 }
